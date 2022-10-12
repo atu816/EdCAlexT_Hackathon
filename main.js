499a3823a5e8495c54a8235f94bc1fe4e5975d6a
atu816 <alexandertu95@gmail.com>
2022-10-12 16:59:04 -0700

    main.js test

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('hello');
+let header = document.querySelector('header');
+let testDiv = document.createElement('div');
+testDiv.setAttribute('style','border: solid red;background-color: grey')
+testDiv.innerText = 'testing'
+header.appendChild(testDiv)
