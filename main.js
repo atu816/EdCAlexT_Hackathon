d953528ab72ad8f5aa63064cc69174d04955a918
atu816 <alexandertu95@gmail.com>
2022-10-12 17:38:35 -0700

    Alex off to dinner

--- main.js
+++ main.js
@@ -1,38 +1,55 @@
-// Wrap in a DOM content loaded
-addEventListener('DOMContentLoaded', (event) => {
-  console.log('hello');
-  let header = document.querySelector('header');
-  let testDiv = document.createElement('div');
-  testDiv.setAttribute('style','border: solid red;background-color: grey')
-  testDiv.innerText = 'testing'
-  header.appendChild(testDiv)
 
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/*
-  if (window.location.href === 'https://developer.mozilla.org/en-US/docs/Web/HTML/') {
-    console.log('HTML page loaded');
+
+const people = [
+  {
+    name: 'Jared',
+    role: 'Instructor',
+    skills: 'Debugging'
+  },
+  {
+    name: 'Kat',
+    role: 'Instructor',
+    skills: 'HTML'
+  },
+  {
+    name: 'Camera',
+    role: 'Instructor',
+    skills: 'OOP'
+  },
+  {
+    name: 'Crystal',
+    role: 'Fellow',
+    skills: 'API'
   }
+];
+// Wrap in a DOM content loaded
+// document.addEventListener('DOMContentLoaded', () => {
+console.log('hello');
+
+let header = document.querySelector('header');
+let testDiv = document.createElement('div');
+
+testDiv.setAttribute('style', 'border: solid red;background-color: grey')
+testDiv.innerText = 'Codesmith Helpers'
+header.appendChild(testDiv)
+console.log(window.location.href);
+if (window.location.href.includes('HTML')) {
+  testDiv.innerText = `${people[0].name} is the ${people[0].role} who specializes in ${people[0].skills}`
+}
+if (window.location.href.includes('CSS')) {
+  testDiv.innerText = `${people[1].name} is the ${people[1].role} who specializes in ${people[1].skills}`
+}
+if (window.location.href.includes('JavaScript')){
+  testDiv.innerText = `${people[2].name} is the ${people[2].role} who specializes in ${people[2].skills}`
+}
+if (window.location.href.includes('API')){
+  testDiv.innerText = `${people[3].name} is the ${people[3].role} who specializes in ${people[3].skills}`
+}
 
 
   // Choose a random person
   // Get people down
     // Render info based on page responded
     // Get URL we are on
-  
-    const people = [
-      {
-        name: 'Jared',
-        role: 'Instructor',
-        skills: 'Debugging'
-      },
-      {
-        name: 'Kat',
-        role: 'Instructor',
-        skills: 'HTML'
-      },
-      {
-        name: 'Camera',
-        role: 'Instructor',
-        skills: 'OOP'
-      }
-    ];
-});
+
+
