31cdda6c4fdb6a1f18a8836eb8616de10a89c268
atu816 <alexandertu95@gmail.com>
2022-10-12 19:33:53 -0700

    Pre database progress

--- main.js
+++ main.js
@@ -1,55 +1,126 @@
-
-
-const people = [
+const resources = [
   {
-    name: 'Jared',
+    name: "Katie Ahn",
+    role: "Fellow",
+    skill: ["JavaScript"]
+},
+  {
+    name: "Jared Lewis",
+    role: "Engineering Instructor",
+    skill: ["HTML", "API"]
+},
+  {
+    name: "Katrina Villanueva",
+    role: "Engineering Instructor",
+    skill: ["CSS", "JavaScript"]
+},
+  {
+    name: "Max Shick",
+    role: "Fellow",
+    skill: ["HTML", "CSS"]
+},
+  {
+    name: "Chris Jamali",
+    role: "Fellow",
+    skill: ["CSS", "HTML", "JavaScript"]
+},
+  {
+    name: 'Kristin Tillotson',
     role: 'Instructor',
-    skills: 'Debugging'
+    skills: ["HTML", "CSS"]
   },
   {
-    name: 'Kat',
-    role: 'Instructor',
-    skills: 'HTML'
-  },
-  {
-    name: 'Camera',
-    role: 'Instructor',
-    skills: 'OOP'
-  },
-  {
-    name: 'Crystal',
+    name: 'Crystal Agoncillo',
     role: 'Fellow',
-    skills: 'API'
+    skills: ['API', 'JavaScript']
+  },
+  {
+    name: 'Matt Dias',
+    role: 'Senior',
+    skills: ['API']
+  },
+  {
+    name: 'Jimmy Lin',
+    role: 'Senior',
+    skills: ['API', "JavaScript"]
   }
 ];
-// Wrap in a DOM content loaded
-// document.addEventListener('DOMContentLoaded', () => {
-console.log('hello');
 
+const cssArr = [];
+const htmlArr = [];
+const jsArr = [];
+const apiArr = [];
+
+// Iterate over
+  // Based on if skill includes certain specialty
+    // Push into respective arr
+    resources.forEach( obj => {
+      Object.keys(obj).forEach( key => {
+        if(obj[key].includes("CSS"))
+        cssArr.push(obj)
+      })
+    })
+    resources.forEach( obj => {
+      Object.keys(obj).forEach( key => {
+        if(obj[key].includes("HTML"))
+        htmlArr.push(obj)
+      })
+    })
+    resources.forEach( obj => {
+      Object.keys(obj).forEach( key => {
+        if(obj[key].includes("JavaScript"))
+        jsArr.push(obj)
+      })
+    })
+    resources.forEach( obj => {
+      Object.keys(obj).forEach( key => {
+        if(obj[key].includes("API"))
+        apiArr.push(obj)
+      })
+    })
+
+    let cssIndex = 0;
+    let htmlIndex = 0;
+    let apiIndex = 0;
+    let jsIndex = 0;
+
+
+// Header ref
 let header = document.querySelector('header');
-let testDiv = document.createElement('div');
+// Toolbar container div
+let toolbar = document.createElement('div');
+toolbar.setAttribute('id', 'toolbar-container');
+// Need help title
+let help = document.createElement('div');
+help.setAttribute('id','help');
+help.innerText = 'Need help?'
+// Container center box
+let toolbarCenter = document.createElement('div');
+toolbarCenter.setAttribute('id', 'center-toolbar');
+// Set default text
+toolbarCenter.innerText = 'Codesmith Helper Bar';
+// Append toolbar text to toolbar, toolbar to header
+toolbar.appendChild(help);
+toolbar.appendChild(toolbarCenter);
+header.appendChild(toolbar);
 
-testDiv.setAttribute('style', 'border: solid red;background-color: grey')
-testDiv.innerText = 'Codesmith Helpers'
-header.appendChild(testDiv)
-console.log(window.location.href);
+// Cases for each window
 if (window.location.href.includes('HTML')) {
-  testDiv.innerText = `${people[0].name} is the ${people[0].role} who specializes in ${people[0].skills}`
+  toolbarCenter.innerText = `${htmlArr[htmlIndex].name} is the ${htmlArr[htmlIndex].role} who specializes in HTML`
+  htmlIndex++;
+  if (htmlIndex === htmlArr.length) htmlIndex = 0;
 }
 if (window.location.href.includes('CSS')) {
-  testDiv.innerText = `${people[1].name} is the ${people[1].role} who specializes in ${people[1].skills}`
+  toolbarCenter.innerText = `${cssArr[cssIndex].name} is the ${cssArr[cssIndex].role} who specializes in CSS`
+  cssIndex++;
+  if (cssIndex === cssArr.length) cssIndex = 0;
 }
 if (window.location.href.includes('JavaScript')){
-  testDiv.innerText = `${people[2].name} is the ${people[2].role} who specializes in ${people[2].skills}`
+  toolbarCenter.innerText = `${jsArr[jsIndex].name} is the ${jsArr[jsIndex].role} who specializes in JavaScript`
+  jsIndex++;
+  if (jsIndex === jsArr.length) jsIndex = 0;
 }
 if (window.location.href.includes('API')){
-  testDiv.innerText = `${people[3].name} is the ${people[3].role} who specializes in ${people[3].skills}`
+  toolbarCenter.innerText = `${apiArr[apiIndex].name} is the ${apiArr[apiIndex].role} who specializes in APIs`
+  if (apiIndex === apiArr.length) apiIndex = 0;
 }
-
-
-  // Choose a random person
-  // Get people down
-    // Render info based on page responded
-    // Get URL we are on
-
-
