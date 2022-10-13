f8015bc4bc130e1bd991bae1ce8d2974d6032032
atu816 <alexandertu95@gmail.com>
2022-10-12 17:19:14 -0700

    Site test

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-console.log('hello');
-let header = document.querySelector('header');
-let testDiv = document.createElement('div');
-testDiv.setAttribute('style','border: solid red;background-color: grey')
-testDiv.innerText = 'testing'
-header.appendChild(testDiv)
+// Wrap in a DOM content loaded
+addEventListener('DOMContentLoaded', (event) => {
+  console.log('hello');
+  let header = document.querySelector('header');
+  let testDiv = document.createElement('div');
+  testDiv.setAttribute('style','border: solid red;background-color: grey')
+  testDiv.innerText = 'testing'
+  header.appendChild(testDiv)
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/*
+  if (window.location.href === 'https://developer.mozilla.org/en-US/docs/Web/HTML/') {
+    console.log('HTML page loaded');
+  }
+
+
+  // Choose a random person
+  // Get people down
+    // Render info based on page responded
+    // Get URL we are on
+  
+    const people = [
+      {
+        name: 'Jared',
+        role: 'Instructor',
+        skills: 'Debugging'
+      },
+      {
+        name: 'Kat',
+        role: 'Instructor',
+        skills: 'HTML'
+      },
+      {
+        name: 'Camera',
+        role: 'Instructor',
+        skills: 'OOP'
+      }
+    ];
+});
